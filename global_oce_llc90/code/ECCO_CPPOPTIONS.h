b0a8d1e5bc0bc7b6f5d96962367bcb7e5d73e929
Gael Forget <gforget@mit.edu>
2014-10-18 14:22:34 +0000

    - remove now un-necessary CPPs and re-organize the others.

--- global_oce_llc90/code/ECCO_CPPOPTIONS.h
+++ global_oce_llc90/code/ECCO_CPPOPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm_contrib/verification_other/global_oce_llc90/code/Attic/ECCO_CPPOPTIONS.h,v 1.14 2014/10/16 20:12:42 gforget Exp $
+C $Header: /u/gcmpack/MITgcm_contrib/verification_other/global_oce_llc90/code/Attic/ECCO_CPPOPTIONS.h,v 1.15 2014/10/18 14:22:34 gforget Exp $
 C $Name:  $
 
 #ifndef ECCO_CPPOPTIONS_H
@@ -7,24 +7,55 @@ C $Name:  $
 #include "PACKAGES_CONFIG.h"
 #include "CPP_OPTIONS.h"
 
-#ifdef ALLOW_ECCO
-
-C CPP flags controlling which code is included in the files that
-C will be compiled.
-C
 C ********************************************************************
 C ***                         ECCO Package                         ***
 C ********************************************************************
 C
 
-C       >>> Do a long protocol.
-#undef ECCO_VERBOSE
+#ifdef ALLOW_ECCO
+
 C       >>> use model/src/forward_step.F
 #define ALLOW_ECCO_EVOLUTION
+
+C       >>> Cost function contributions
+#define ALLOW_ECCO_OLD_FC_PRINT
+
+C       >>> ALLOW_GENCOST_CONTRIBUTION: interactive way to add basic 2D cost function terms.
+C       > In data.ecco, this requires the specification of data file (name, frequency,
+C         etc.), bar file name for corresp. model average, standard error file name, etc.
+C       > In addition, adding such cost terms requires editing ecco_cost.h to increase
+C         NGENCOST, and editing cost_gencost_customize.F to implement the actual
+C         model average (i.e. the bar file content).
+#define ALLOW_GENCOST_CONTRIBUTION
+C       >>> free form version of GENCOST: allows one to use otherwise defined elements (e.g.
+C         psbar and and topex data) while taking advantage of the cost function/namelist slots
+C         that can be made available using ALLOW_GENCOST_CONTRIBUTION. To this end
+C         ALLOW_GENCOST_CONTRIBUTION simply switches off tests that check whether all of the
+C         gencost elements (e.g. gencost_barfile and gencost_datafile) are specified in data.ecco.
+C       > While this option increases flexibility within the gencost framework, it implies more room
+C         for error, so it should be used cautiously, and with good knowledge of the rest of pkg/ecco.
+C       > It requires providing a specific cost function routine, and editing cost_gencost_all.F accordingly.
+#define ALLOW_GENCOST_FREEFORM
+C       >>> 3 dimensional version of GENCOST:
+#define ALLOW_GENCOST3D
+
+c in case there is a single observational file (rather than yearly files)
+c assume it contains a climatology (otherwise, assume it is a full time series)
+#define COST_GENERIC_ASSUME_CYCLIC
+
+c include global mean steric sea level correction in etanFull
+#define ALLOW_PSBAR_STERIC
+
+C       >>> In-Situ Profiles.
+#define ALLOW_PROFILES_CONTRIBUTION
+
+#endif /* ALLOW_ECCO */
 
 C ********************************************************************
 C ***                  Adjoint Support Package                     ***
 C ********************************************************************
+
+#ifdef ALLOW_AUTODIFF
 
 #define ALLOW_AUTODIFF_TAMC
 C
@@ -46,58 +77,15 @@ C o use divided adjoint to split adjoint computations
 #define EXCLUDE_WHIO_GLOBUFF_2D
 #define ALLOW_INIT_WHTAPEIO
 
-C ********************************************************************
-C ***                     Calender Package                         ***
-C ********************************************************************
-C 
-C CPP flags controlling which code is included in the files that
-C will be compiled.
-
-CPH >>>>>> THERE ARE NO MORE CAL OPTIONS TO BE SET <<<<<<
-
-C ********************************************************************
-C ***                Cost function Package                         ***
-c ********************************************************************
-C 
-
-C       >>> Cost function contributions
-#define ALLOW_ECCO_OLD_FC_PRINT
-
-C       >>> ALLOW_GENCOST_CONTRIBUTION: interactive way to add basic 2D cost function terms.
-C       > In data.ecco, this requires the specification of data file (name, frequency,
-C         etc.), bar file name for corresp. model average, standard error file name, etc.
-C       > In addition, adding such cost terms requires editing ecco_cost.h to increase
-C         NGENCOST, and editing cost_gencost_customize.F to implement the actual
-C         model average (i.e. the bar file content).
-#define ALLOW_GENCOST_CONTRIBUTION
-C       >>> free form version of GENCOST: allows one to use otherwise defined elements (e.g.
-C         psbar and and topex data) while taking advantage of the cost function/namelist slots
-C         that can be made available using ALLOW_GENCOST_CONTRIBUTION. To this end
-C         ALLOW_GENCOST_CONTRIBUTION simply switches off tests that check whether all of the
-C         gencost elements (e.g. gencost_barfile and gencost_datafile) are specified in data.ecco.
-C       > While this option increases flexibility within the gencost framework, it implies more room
-C         for error, so it should be used cautiously, and with good knowledge of the rest of pkg/ecco.
-C       > It requires providing a specific cost function routine, and editing cost_gencost_all.F accordingly.
-#define ALLOW_GENCOST_FREEFORM
-
-#define COST_GENERIC_ASSUME_CYCLIC
-
-#define ALLOW_GENCOST3D
-
-#define ALLOW_GENCOST_SSHV4_OUTPUT
-# undef ALLOW_SHALLOW_ALTIMETRY
-# undef ALLOW_HIGHLAT_ALTIMETRY
-#define ALLOW_PSBAR_MEAN
-#define ALLOW_PSBAR_STERIC
-#define ALLOW_GENCOST_SEAICEV4
-
-C       >>> In-Situ Profiles.
-#define ALLOW_PROFILES_CONTRIBUTION
+#endif /* ALLOW_AUTODIFF */
 
 C ********************************************************************
 C ***               Control vector Package                         ***
 C ********************************************************************
 C 
+
+#ifdef ALLOW_CTRL
+
 # undef  CTRL_SET_OLD_MAXCVARS_30
 #define  CTRL_SET_PREC_32
 #define  ALLOW_NONDIMENSIONAL_CONTROL_IO
@@ -117,6 +105,7 @@ C       >>> Spatial Correlation Operator.
 C       >>> rotation of xx for wind
 #define ALLOW_ROTATE_UV_CONTROLS
 
-#endif /* ALLOW_ECCO */
+#endif /* ALLOW_CTRL */
+
 #endif /* ECCO_CPPOPTIONS_H */
 
