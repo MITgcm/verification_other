94da318cf36a4fb165271407d5ad421c35ab5f3f
Gael Forget <gforget@mit.edu>
2015-11-21 18:27:57 +0000

    - illustrate and test vertical and temporal averaging   specs; cost function change where boxmean is mulitplied   by 1.D+07 for more efficient testing in addition to transp.

--- global_oce_cs32/input_ad.sens/some_mask.m
+++ global_oce_cs32/input_ad.sens/some_mask.m
@@ -2,18 +2,25 @@
 gcmfaces_global;
 grid_load('../run/',6,'compact');
 
-mskW=convert2gcmfaces(mygrid.LATS_MASKS(100).mskWedge);
-mskS=convert2gcmfaces(mygrid.LATS_MASKS(100).mskSedge);
-mskW(isnan(mskW))=0;
-mskS(isnan(mskS))=0;
-write2file('some_maskW',mskW);
-write2file('some_maskS',mskS);
+if 0;
+    mskW=convert2gcmfaces(mygrid.LATS_MASKS(100).mskWedge);
+    mskS=convert2gcmfaces(mygrid.LATS_MASKS(100).mskSedge);
+    mskW(isnan(mskW))=0;
+    mskS(isnan(mskS))=0;
+    write2file('north10_maskW',mskW);
+    write2file('north10_maskS',mskS);
+end;
 
-msk=convert2gcmfaces(mygrid.mskC);
-msk(isnan(msk))=0;
-write2file('some_maskC',msk);
+if 0;
+    msk=mygrid.YC<-30;    
+    msk=convert2gcmfaces(msk.*mygrid.mskC(:,:,1));
+    msk(isnan(msk))=0;
+    write2file('south30_maskC',msk);
+    %
+    msk=ones(1,50); msk(11:end)=0;
+    write2file('south30_maskK',msk);
+    msk=zeros(1,10); msk(5)=1;
+    write2file('south30_maskT',msk);
+end;
 
-msk=convert2gcmfaces(mygrid.mskC(:,:,1));
-msk(isnan(msk))=0;
-write2file('some_maskCsurf',msk);
 
