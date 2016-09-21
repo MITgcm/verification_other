1f38da8a8fa48ed31c653fa0fd6e5330e3112256
Gael Forget <gforget@mit.edu>
2016-09-21 19:20:02 +0000

    - update and cleanup

--- global_oce_cs32/code/ECCO_OPTIONS.h
+++ global_oce_cs32/code/ECCO_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm_contrib/verification_other/global_oce_cs32/code/ECCO_OPTIONS.h,v 1.1 2016/05/29 12:37:27 gforget Exp $
+C $Header: /u/gcmpack/MITgcm_contrib/verification_other/global_oce_cs32/code/ECCO_OPTIONS.h,v 1.2 2016/09/21 19:20:02 gforget Exp $
 C $Name:  $
 
 CBOP
@@ -22,49 +22,33 @@ CEOP
 #ifdef ECCO_CPPOPTIONS_H
 
 C-- When multi-package option-file ECCO_CPPOPTIONS.h is used (directly included
-C    in CPP_OPTIONS.h), this option file is left empty since all options that
+C   in CPP_OPTIONS.h), this option file is left empty since all options that
 C   are specific to this package are assumed to be set in ECCO_CPPOPTIONS.h
 
 #else /* ndef ECCO_CPPOPTIONS_H */
-C   ==================================================================
+
 C-- Package-specific Options & Macros go here
-C
 
-C       >>> Cost function contributions
-#define ALLOW_ECCO_OLD_FC_PRINT
-
-C       >>> ALLOW_GENCOST_CONTRIBUTION: interactive way to add basic 2D cost function terms.
-C       > In data.ecco, this requires the specification of data file (name, frequency,
-C         etc.), bar file name for corresp. model average, standard error file name, etc.
-C       > In addition, adding such cost terms requires editing ecco_cost.h to increase
-C         NGENCOST, and editing cost_gencost_customize.F to implement the actual
-C         model average (i.e. the bar file content).
+C allow for generic cost function and integral terms
 #define ALLOW_GENCOST_CONTRIBUTION
-C       >>> free form version of GENCOST: allows one to use otherwise defined elements (e.g.
-C         psbar and and topex data) while taking advantage of the cost function/namelist slots
-C         that can be made available using ALLOW_GENCOST_CONTRIBUTION. To this end
-C         ALLOW_GENCOST_CONTRIBUTION simply switches off tests that check whether all of the
-C         gencost elements (e.g. gencost_barfile and gencost_datafile) are specified in data.ecco.
-C       > While this option increases flexibility within the gencost framework, it implies more room
-C         for error, so it should be used cautiously, and with good knowledge of the rest of pkg/ecco.
-C       > It requires providing a specific cost function routine, and editing cost_gencost_all.F accordingly.
-#define ALLOW_GENCOST_FREEFORM
-C       >>> 3 dimensional version of GENCOST:
+C allow for 3 dimensional generic terms
 #define ALLOW_GENCOST3D
 
-c in case there is a single observational file (rather than yearly files)
-c assume it contains a climatology (otherwise, assume it is a full time series)
-#define COST_GENERIC_ASSUME_CYCLIC
-
-c include global mean steric sea level correction in etanFull
+C include global mean steric sea level correction
 #define ALLOW_PSBAR_STERIC
+C allow for near-shore and high-latitude altimetry
 #define ALLOW_SHALLOW_ALTIMETRY
 #define ALLOW_HIGHLAT_ALTIMETRY
 
-C       >>> In-Situ Profiles.
+C allow for In-Situ Profiles cost function contribution
 #define ALLOW_PROFILES_CONTRIBUTION
 
-C   ==================================================================
+C cost function output format
+#define ALLOW_ECCO_OLD_FC_PRINT
+
+C re-activate deprecated codes (just in case ... but not recommended)
+#undef ECCO_CTRL_DEPRECATED
+
 #endif /* ndef ECCO_CPPOPTIONS_H */
 #endif /* ALLOW_ECCO */
 #endif /* ECCO_OPTIONS_H */
