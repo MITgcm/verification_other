cf43f3c58a92e4c0b2c7bc81302aa5f8c5222635
Jean-Michel Campin <jmc@mit.edu>
2025-10-18 11:11:11 -0400

    simplify: single SST file for all 3 tests
    - use the same target SST file for all 3 tests so that secondary test "ctrl" gives
      same results as the 2 other tests (no split between trend and saisonal cycle -
      -> same machine truncation ; and just load a 1 record constant zero value for
      pkg/exf climsst).
    - adjust some numerical value to round number (rhoConst, Cp, climTempFreeze, ...)

--- small_toy/input/gendata.m
+++ small_toy/input/gendata.m
@@ -20,37 +20,33 @@ if kwr > 0,
  fprintf(' - writing file "%s" ...',fNam);
  fid=fopen(fNam,'w',typ); fwrite(fid,var,prec); fclose(fid);
  fprintf(' done\n');
+
+ fNam='zero_64b.bin'; var=0.;
+ fprintf(' - writing file "%s" ...',fNam);
+ fid=fopen(fNam,'w',typ); fwrite(fid,var,prec); fclose(fid);
+ fprintf(' done\n');
 end
 
 %- SST relaxation:
-nRec=100; dTsais=4;
-t0=[1:nRec];
+nRec=100; dTsais=2; %- amplitude of seasonal cycle
+t0=[0:nRec-1]; %- linear trend
 t1=ones(2,nRec/2); t1(1,:)=-1.; t1=reshape(t1,[1 nRec]);
-t1=t1*0.5*dTsais;
+t1=t1*dTsais;
 sst=t0+t1;
 
 figure(1); clf;
- xax=([1:nRec]-0.5)*0.5; %- time in years
+ xax=([1:nRec]-1)*0.5; %- time in years
  plot(xax,t0,'k-');
  hold on
  plot(xax,t1,'b-');
  plot(xax,sst,'r-');
  hold off
  grid
- axis([0 50 -5 105]);
+%axis([0 50 -3 103]); %- full time-serie
+ axis([0 6 -3 13.]);  %- first 6 yrs
+ xlabel('time [yr]');
+ title('relaxation SST');
 
-if kwr > 0,
-%- split seasonal cycle and (linear) trend (for pkg/exf and pkg/ctrl)
- fNam=['trend_SST.',int2str(nRec),'.bin']; var=t0;
- fprintf(' - writing file "%s" ...',fNam);
- fid=fopen(fNam,'w',typ); fwrite(fid,var,prec); fclose(fid);
- fprintf(' done\n');
-
- fNam=['saison_SST.',int2str(nRec),'.bin']; var=t1;
- fprintf(' - writing file "%s" ...',fNam);
- fid=fopen(fNam,'w',typ); fwrite(fid,var,prec); fclose(fid);
- fprintf(' done\n');
-end
 if kwr > 0,
 %- seasonal cycle and (linear) trend all together (for pkg/exf)
  fNam=['target_SST.',int2str(nRec),'.bin']; var=sst;
